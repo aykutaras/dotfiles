8186f4cd2f7f8661c80192726ad1b1d72a0ac004
Aykut Aras <aykutaras@gmail.com>
2024-11-28 09:42:10 +0100

    updated the mac related stuff

--- mac/install.sh
+++ mac/install.sh
@@ -1,16 +1,13 @@
 cp .zshrc ~/
-mkdir ~/Projects
 
-brew install awscli jq go zsh-completions terraform kubectl minikube eksctl openjdk jmeter gh
+brew install awscli jq zsh-completions terraform
 brew install --cask iterm2
 brew install --cask amethyst
-brew install --cask postman
 brew install --cask slack
 brew install --cask spotify
 brew install --cask the-unarchiver
 brew install --cask visual-studio-code
 brew install --cask vnc-viewer
-brew install --cask owasp-zap
 
 # https://stackoverflow.com/questions/39972335/how-do-i-press-and-hold-a-key-and-have-it-repeat-in-vscode
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
--- vim/.vimrc
+++ vim/.vimrc
@@ -4,9 +4,20 @@ filetype plugin indent on
 set backspace=2
 set ruler
 set tabstop=4
+set softtabstop=4
 set shiftwidth=4
 set expandtab
+set autoindent
+set fileformat=unix
 set background=dark
 set re=0
+set encoding=utf-8
+"line numbers
+set nu
 
-colorscheme solarized
+"This will mark extra whitespace as bad and probably color it red.
+"au BufRead,BufNewFile *.py,*.pyw,*.c,*.h match BadWhitespace /\s\+$/
+
+let g:ycm_autoclose_preview_window_after_completion=1
+map <leader>g  :YcmCompleter GoToDefinitionElseDeclaration<CR>
+
